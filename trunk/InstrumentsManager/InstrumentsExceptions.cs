958b1359be54f7a5fb40e7a2a19ab03a465caf29
miltondp <miltondp@b7d2772f-5a2f-0410-9352-9debd1fbfd55>
2007-05-10 20:02:29 +0000

    - Migration of the files of Zazpe to ZaspeSharp. - SharpDevelop and MonoDevelop project and solutions files added. - All functions, variables, clases are translated from spanish to english, but comments stay in spanish yet.

---
+++ trunk/InstrumentsManager/InstrumentsExceptions.cs
@@ -0,0 +1,41 @@
+﻿/* 
+   Zaspe# - Attendance management
+   Copyright (C) 2006, 2007 Milton Pividori
+
+   Zaspe# is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   Zaspe# is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Zaspe#; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+
+namespace ZaspeSharp.Instruments
+{
+	public class InstrumentException : System.Exception
+	{
+		public InstrumentException() : base("Hubo un error con algún instrumento") {}
+		public InstrumentException(string strErr) : base(strErr) {}
+	}
+	
+	public class InstrumentExistsException : InstrumentException
+	{
+		public InstrumentExistsException() : base("Ya existe un instrumento con ese nombre") {}
+		public InstrumentExistsException(string strErr) : base(strErr) {}
+	}
+	
+	public class InstrumentDoesNotExistException : InstrumentException
+	{
+		public InstrumentDoesNotExistException() : base("No existe un instrumento con ese nombre") {}
+		public InstrumentDoesNotExistException(string strErr) : base(strErr) {}
+	}
+}
---
+++ trunk/PersonsManager/PersonsManager.Methods.cs
@@ -0,0 +1,116 @@
+﻿/* 
+   Zaspe# - Attendance management
+   Copyright (C) 2006, 2007 Milton Pividori
+
+   Zaspe# is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   Zaspe# is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Zaspe#; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+using System;
+using System.Diagnostics;
+using System.Collections;
+using Gentle.Framework;
+
+namespace ZaspeSharp.Persons
+{
+	public partial class PersonsManager
+	{
+		#region Retrieve methods
+		/// <summary>
+		/// Recupera una persona por su id o DNI
+		/// </summary>
+		/// <param name="dni_id">Puede ser el id o DNI. Si falla con uno trata con el otro.</param>
+		/// <returns></returns>
+		public Person Retrieve(int dni_id)
+		{
+			// I look for a person by his DNI
+			Key key = new Key(typeof(Person), true);
+			key.Add("dni", dni_id);
+			
+			Person aPerson = Broker.SessionBroker.TryRetrieveInstance(typeof(Person), key) as Person;
+			
+			// If found, it's returned
+			if (aPerson != null)
+				return aPerson;
+
+			// Now i look for by id
+			key = new Key(typeof(Person), true);
+			key.Add("id", dni_id);
+			
+			aPerson = Broker.SessionBroker.TryRetrieveInstance(typeof(Person), key) as Person;
+			
+			if (aPerson != null)
+				return aPerson;
+			
+			// the person who is looked for does not exist
+			throw new PersonDoesNotExistException("No existe una persona con ese DNI");
+		}
+		
+		public Person[] RetrieveAll()
+		{
+			// Recuperamos todas las personas de la base de datos
+			SqlBuilder sb = new SqlBuilder(StatementType.Select, typeof(Person));
+			SqlStatement stmt = sb.GetStatement(true);
+
+			Person[] persons = (Person[])ObjectFactory.GetCollection(typeof(Person), stmt.Execute());
+			return persons;
+		}
+		#endregion
+		
+		#region Entry methods
+		public Person AddPerson(int dni, string surname, string name, bool is_man,
+		                               bool data_is_complete)
+		{
+			// Chequemos que no exista una persona con el mismo DNI
+			try {
+				Retrieve(dni);
+			}
+			catch (PersonDoesNotExistException) {
+				Person newPerson = new Person(dni, surname, name, is_man, data_is_complete);
+				newPerson.Persist();
+				
+				return newPerson;
+			}
+			
+			throw new PersonExistsException("Se está intentando ingresar una persona que" +
+					" ya existe en la base de datos.");
+		}
+		#endregion
+		
+		#region Deletion methods
+		public void DeletePerson(int dni)
+		{
+			Person aPerson;
+			
+			try {
+				aPerson = Retrieve(dni);
+			}
+			catch (PersonDoesNotExistException) {
+				throw new PersonDoesNotExistException("Se está intentando eliminar una persona que" + 
+					" no existe en la base de datos.");
+			}
+			
+			aPerson.Remove();
+		}
+		#endregion
+				
+		#region Other methods
+		[Conditional("DEBUG")]
+		public void CleanCache()
+		{
+			Gentle.Common.CacheManager.Clear();
+		}
+		#endregion
+	}
+}
