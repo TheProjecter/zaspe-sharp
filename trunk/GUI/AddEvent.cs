7f82de9c2a8db218d267cd30bf0268ab677e5f49
miltondp <miltondp@b7d2772f-5a2f-0410-9352-9debd1fbfd55>
2007-05-14 21:03:54 +0000

    - AddPerson and AddEvent GUIs (glade) added.

---
+++ trunk/GUI/AddEvent.cs
@@ -0,0 +1,37 @@
+
+using System;
+using Glade;
+using Gtk;
+
+namespace ZaspeSharp.GUI
+{
+	public class AddEvent
+	{
+		[Widget]
+		Dialog dlgAddEvent;
+		
+		public AddEvent(Window parent)
+		{
+			Glade.XML gxml = new Glade.XML ("add_event.glade", "dlgAddEvent", null);
+			gxml.Autoconnect(this);
+			
+			this.dlgAddEvent.TransientFor = parent;
+			this.dlgAddEvent.Show();
+		}
+		
+		public void OnCancelClicked(object o, EventArgs args)
+		{
+			this.dlgAddEvent.Destroy();
+		}
+		
+		public void OnOkAddClicked(object o, EventArgs args)
+		{
+		
+		}
+		
+		public void OnOkCloseClicked(object o, EventArgs args)
+		{
+		
+		}
+	}
+}
