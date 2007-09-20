9da86bc752aee049c1c85d45bc7150058a892138
miltondp <miltondp@b7d2772f-5a2f-0410-9352-9debd1fbfd55>
2007-09-20 21:06:21 +0000

    - Tests with Cairo, GtkPrint, Rsvg and images.

--- trunk/zaspe-sharp/MonoReporter/Report.cs
+++ trunk/zaspe-sharp/MonoReporter/Report.cs
@@ -17,14 +17,57 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using Gtk;
 
 namespace MonoReporter
 {
 	public class Report
 	{
+		PrintOperation printOperation;
 		
 		public Report()
 		{
+			// All this is only a test.
+			
+			Console.WriteLine("uno");
+			this.printOperation = new PrintOperation();
+			
+			this.printOperation.ShowProgress = true;
+			printOperation.NPages = 1;
+			printOperation.Unit = Unit.Mm;
+			printOperation.JobName = "ImagenesEnGtkPrint";
+			
+//			PrintSettings settings = new PrintSettings();
+//			settings.UseColor = true;
+//			printOperation.PrintSettings = settings;
+			
+			Console.WriteLine("dos");
+			printOperation.DrawPage += this.DrawPage;
+		}
+		
+		public void DrawPage(object o, DrawPageArgs args)
+		{
+			Cairo.Context con = args.Context.CairoContext;
+			con.Antialias = Cairo.Antialias.Subpixel;
+			
+			// Cargo una imagen
+			Console.WriteLine("cuatro");
+			Gdk.Pixbuf imagen = Rsvg.Pixbuf.FromFileAtSize("prueba.svg", (int)args.Context.Width, (int)args.Context.Height);
+			
+			Gdk.CairoHelper.SetSourcePixbuf(con, imagen, 0, 0);
+			con.Paint();
+		}
+		
+		public void Run(Gtk.Window win)
+		{
+			Console.WriteLine("tres");
+			printOperation.Run(PrintOperationAction.PrintDialog, win);
+			
+//			if (res == PrintOperationResult.Apply) {
+//				Console.WriteLine("Se apretó Apply");
+//			}
+//			else
+//				Console.WriteLine("ajajajajaj");
 		}
 	}
 }
