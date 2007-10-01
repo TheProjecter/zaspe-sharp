b5ebf661bfa8a0b0f4cacde1b790933d3c4abb58
miltondp <miltondp@b7d2772f-5a2f-0410-9352-9debd1fbfd55>
2007-10-01 14:18:35 +0000

    - GUI improvements. - Added GtkAssistant to make reports (Development).

---
+++ trunk/zaspe-sharp/GUI/AttendancesReport.cs
@@ -0,0 +1,26 @@
+// /home/miltondp/Projects/zaspe-sharp/GUI/AttendancesReport.cs created with MonoDevelop
+// User: miltondp at 20:25 04/08/2007
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+using Gtk;
+using Glade;
+
+namespace ZaspeSharp.GUI
+{
+	public class AttendancesReport
+	{
+		Assistant attendancesReportAssistant;
+		
+		public AttendancesReport(Gtk.Window parent)
+		{
+			this.attendancesReportAssistant = new Assistant();
+			
+			this.attendancesReportAssistant.Parent = parent;
+			
+			this.attendancesReportAssistant.ShowAll();
+		}
+	}
+}
