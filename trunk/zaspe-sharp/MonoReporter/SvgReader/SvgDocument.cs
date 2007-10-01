dc51524621c8d4c55de36808fe953d55a2992899
miltondp <miltondp@b7d2772f-5a2f-0410-9352-9debd1fbfd55>
2007-10-01 03:07:13 +0000

    Now it reads the stroke-width attribute correctly from rectangles

--- trunk/zaspe-sharp/MonoReporter/SvgReader/SvgDocument.cs
+++ trunk/zaspe-sharp/MonoReporter/SvgReader/SvgDocument.cs
@@ -1,19 +1,22 @@
-//    MensajeroRemoting - Mensajero instantáneo hecho con .NET Remoting
-//    y otras tecnologías de .NET.
-//    Copyright (C) 2007  Luis Ignacio Larrateguy, Milton Pividori y César Sandrigo
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+  MonoReporter - Report tool for Mono
+  Copyright (C) 2006,2007 Milton Pividori
+
+  This file is part of MonoReporter.
+
+  MonoReporter is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  MonoReporter is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 using System;
 using System.Globalization;
@@ -48,7 +51,7 @@ namespace SvgReader
 					return this.pageWidth;
 				
 				XmlNode svgNode = this.xmlDocument.GetElementsByTagName("svg")[0];
-				string attributeValue = this.GetAttributeValueFromNode(svgNode, "width");
+				string attributeValue = Utils.GetAttributeValueFromNode(svgNode, "width");
 				
 				string widthValue;
 				if (attributeValue.EndsWith("mm")) {
@@ -73,7 +76,7 @@ namespace SvgReader
 					return this.pageHeight;
 				
 				XmlNode svgNode = this.xmlDocument.GetElementsByTagName("svg")[0];
-				string attributeValue = this.GetAttributeValueFromNode(svgNode, "height");
+				string attributeValue = Utils.GetAttributeValueFromNode(svgNode, "height");
 				
 				string heightValue;
 				if (attributeValue.EndsWith("mm")) {
@@ -97,44 +100,11 @@ namespace SvgReader
 				List<Rectangle> rectangles = new List<Rectangle>();
 				XmlNodeList rectanglesList = this.xmlDocument.GetElementsByTagName("rect");
 				
-				Rectangle aRectangle;
 				foreach (XmlNode rectNode in rectanglesList)
-				{
-					string id = this.GetAttributeValueFromNode(rectNode, "id");
-					string width = this.GetAttributeValueFromNode(rectNode, "width");
-					string height = this.GetAttributeValueFromNode(rectNode, "height");
-					string x = this.GetAttributeValueFromNode(rectNode, "x");
-					string y = this.GetAttributeValueFromNode(rectNode, "y");
-					string lineWidth = this.GetAttributeValueFromNode(rectNode, "style");
-					
-					aRectangle = new Rectangle(id,
-					                           Utils.PixelToMm(Utils.DoubleParse(width)),
-					                           Utils.PixelToMm(Utils.DoubleParse(height)),
-					                           Utils.PixelToMm(Utils.DoubleParse(x)),
-					                           Utils.PixelToMm(Utils.DoubleParse(y)));
-					
-					rectangles.Add(aRectangle);
-				}
+					rectangles.Add(new Rectangle(rectNode));
 				
 				return rectangles.ToArray();
 			}
-		}
-		
-		private string GetAttributeValueFromNode(XmlNode node, string attributeName)
-		{
-			foreach (XmlAttribute anAttribute in node.Attributes) {
-				if (!anAttribute.Name.Equals(attributeName))
-					continue;
-				
-				return anAttribute.Value;
-			}
-			
-			throw new Exception("'" + attributeName + "'" + " attribute not found.");
-		}
-		
-		private string GetValueFromMultiValuatedAttribute(XmlNode node, string attributeName, string subOption)
-		{
-			return null;
 		}
 	}
 }
--- trunk/zaspe-sharp/MonoReporter/SvgReader/Utils.cs
+++ trunk/zaspe-sharp/MonoReporter/SvgReader/Utils.cs
@@ -1,30 +1,37 @@
-//  MonoReporter - Report tool for Mono
-//  Copyright (C) 2006,2007 Milton Pividori
-//
-//  This file is part of MonoReporter.
-//
-//  MonoReporter is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  MonoReporter is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+  MonoReporter - Report tool for Mono
+  Copyright (C) 2006,2007 Milton Pividori
+
+  This file is part of MonoReporter.
+
+  MonoReporter is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  MonoReporter is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 using System;
+using System.Text.RegularExpressions;
+using System.Xml;
 using System.Globalization;
 
 namespace SvgReader
 {
 	public static class Utils
 	{
-		public static System.Globalization.NumberFormatInfo numberFormat;
+		public static NumberFormatInfo numberFormat;
+		
 		public const double pxToMmConversion = 0.282258065;
+		private const char subOptionsSeparator = ';';
+		private const char subOptionFromValueSeparator = ':';
 		
 		static Utils()
 		{
@@ -40,6 +47,36 @@ namespace SvgReader
 		public static double DoubleParse(string doubleString)
 		{
 			return Double.Parse(doubleString, numberFormat);
+		}
+		
+		public static double DoubleParseAndPixelToMm(string doubleString)
+		{
+			double d = Utils.DoubleParse(doubleString);
+			return Utils.PixelToMm(d);
+		}
+		
+		public static string GetAttributeValueFromNode(XmlNode node, string attributeName)
+		{
+			foreach (XmlAttribute anAttribute in node.Attributes) {
+				if (anAttribute.Name.Equals(attributeName))
+					return anAttribute.Value;
+			}
+			
+			throw new Exception("'" + attributeName + "'" + " attribute not found.");
+		}
+		
+		public static string GetValueFromMultiValuatedAttribute(XmlNode node, string attributeName, string subOptionName)
+		{
+			string attributeValue = Utils.GetAttributeValueFromNode(node, attributeName);
+			
+			foreach (string aSubOption in attributeValue.Split(subOptionsSeparator)) {
+				string[] subOptionItems = aSubOption.Split(subOptionFromValueSeparator);
+				
+				if (subOptionItems[0].Equals(subOptionName))
+					return subOptionItems[1];
+			}
+			
+			throw new SubOptionNotFoundException("'" + subOptionName + "'" + " sub option not found.");
 		}
 	}
 }
