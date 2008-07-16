178e9e39fe02a06a56a3572824f2e7e703495d8b
miltondp <miltondp@b7d2772f-5a2f-0410-9352-9debd1fbfd55>
2008-07-16 01:01:05 +0000

    0.1.0 version with Report Generation!
    * ReportGenerator/ReportTypes/SimpleAttendanceList.cs,
      ReportGenerator/ReportTypes/SimplePersonsList.cs,
      ReportGenerator/ReportGenerator.mdp, ReportGenerator/ReportType.cs,
      ReportGenerator/ReportGUI.cs: Report generation finished for
      persons and attendances (events report is missing).
    * GUI/AssemblyInfo.cs: 0.1.0 version.
    * GUI/GUI.mdp: Now uses gtk 2.10 for compatibility with Windows
      systems.
    * GUI/glade/main_window.glade: Changes in the size of the main window.

--- zaspe-sharp/GUI/AssemblyInfo.cs
+++ zaspe-sharp/GUI/AssemblyInfo.cs
@@ -46,4 +46,4 @@ using System.Runtime.InteropServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("0.0.3")]
+[assembly: AssemblyVersion("0.1.0")]
